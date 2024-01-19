e53a5cf38773650d6687da63e711578797b70a39
Serge-boro <serhiiborodin83@gmail.com>
2024-01-18 20:33:52 -0500

    added netlify

---
+++ functions/keys/keys.dev.js
@@ -0,0 +1,10 @@
+const password = 'W05w0SOgG8J4P3QR'
+//mongodb+srv://SerhiiBorodin:W05w0SOgG8J4P3QR@cluster0.qmn1t.mongodb.net/StoreProject
+module.exports = {
+  MongoDB_URI: `mongodb+srv://SerhiiBorodin:${password}@cluster0.qmn1t.mongodb.net/StoreProject`,
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY:
+    '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',
+  EMAIL_FROM: 'serhiiborodin83@gmail.com',
+  BASE_URL: 'http://localhost:3000',
+}
